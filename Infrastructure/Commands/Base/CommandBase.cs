f87b115cad64f5bc5ec44cbaf0027ad7b1b41a86
Viktor Eismont <veism@softserveinc.com>
2021-11-02 17:04:47 +0200

    project initialize commit

---
+++ Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TravelManager.Infrastructure.Commands.Base
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+        
+    }
+}
---
+++ Infrastructure/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TravelManager.Infrastructure.Commands.Base;
+
+namespace TravelManager.Infrastructure.Commands
+{
+    public class Command : CommandBase
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public Command(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
