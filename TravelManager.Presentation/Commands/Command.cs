f10dbd214a5d3ac92505d02e4d758567d5fe8b81
Viktor Eismont <veism@softserveinc.com>
2021-11-03 16:59:48 +0200

    fixed basic project architecture

---
+++ TravelManager.Presentation/Commands/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TravelManager.Presentation.Commands
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        protected virtual bool CanExecute(object parameter) => true;
+
+        protected abstract void Execute(object parameter);
+
+        protected virtual void OnCanExecuteChanged(EventArgs e)
+        {
+            CanExecuteChanged?.Invoke(this, e);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged(EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute(parameter);
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute(parameter);
+        }
+    }
+}
